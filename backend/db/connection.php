e13beb5dde0dbc78d319413cdf2cff021d7409ea
Nacho <nachoag82@gmail.com>
2025-05-26 19:51:06 +0200

    v2.1 cambios en la config de bbdd para el deploy

--- backend/db/connection.php
+++ backend/db/connection.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost'; // Database host
-$dbname = 'RF'; // Cambia esto por el nombre real de tu base de datos
-$username = 'root'; // Cambia esto por tu usuario real de MySQL
-$password = ''; // Cambia esto por tu contraseña real de MySQL
+$host = 'db5017917597.hosting-data.io'; 
+$dbname = 'dbs14265138'; 
+$username = 'dbu2826295'; 
+$password = 'Colegioesclavassevilla2016'; 
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
